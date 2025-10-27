3e81a30072756626c72acafc37ed9f4371b2ccf2
Vontha Chan <89168934+vonthachan@users.noreply.github.com>
2025-10-26 22:44:19 -0500

    Initial commit: Spring Boot Project Setup

---
+++ src/main/java/com/voon/taskflow/TaskFlowApplication.java
@@ -0,0 +1,13 @@
+package com.voon.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFlowApplication.class, args);
+	}
+
+}
